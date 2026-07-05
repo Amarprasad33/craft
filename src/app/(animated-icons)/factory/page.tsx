b6df75872bdf4fcc96d2354a520c2d3225a46121
Amar Prasad <amarp4578@gmail.com>
2026-07-05 14:36:49 +0530

    factory v1 done

--- src/app/(animated-icons)/factory/page.tsx
+++ src/app/(animated-icons)/factory/page.tsx
@@ -1,69 +1,107 @@
 "use client";
 
-import { animate, motion, useMotionValue, useTransform } from "motion/react";
+import { animate, AnimatePresence, easeOut, motion, useAnimate, useMotionValue, useTransform } from "motion/react";
+import { useEffect } from "react";
 
 export default function FactoryPage() {
     return (
         <>
             <div className="flex bg-zinc-200 min-h-screen w-full flex-col items-center justify-center overflow-x-hidden">
-                <FactoryIcon />
+                {/* <FactoryIcon /> */}
+                <FactoryIconV2 />
             </div>
         </>
     )
 }
 
-const CHIMNEY_PATHS = [
-    "M3.55514 18.5369C3.76928 16.2133 5.71845 14.4355 8.05188 14.4355C10.3762 14.4355 12.3209 16.1999 12.5464 18.5132L13.9452 32.8634C14.0783 34.2287 13.0053 35.4114 11.6336 35.4114H4.54647C3.17957 35.4114 2.10825 34.2368 2.23369 32.8756L3.55514 18.5369Z",
-    "M3.3096 21.4439C3.5487 19.1513 5.4559 17.4114 7.7299 17.4114C9.9937 17.4114 11.8958 19.1362 12.1473 21.4169L13.4059 32.8308C13.5576 34.2071 12.4952 35.4114 11.129 35.4114H4.3994C3.0388 35.4114 1.978 34.2164 2.121 32.8447L3.3096 21.4439Z",
-] as const;
 
-const SQUEEZE_TRANSITION = {
-    type: "spring" as const,
-    stiffness: 320,
-    bounce: 0.28,
-    mass: 0.75,
-};
+const FactoryIconV2 = () => {
+    const [scope, animate] = useAnimate();
 
-const RELEASE_TRANSITION = {
-    type: "spring" as const,
-    stiffness: 180,
-    bounce: 0.58,
-    mass: 1.1,
-};
+    const CHIMNEY_PATHS = [
+        "M4.53675 22.0367C4.74384 19.7507 6.66028 18 8.95564 18C11.2422 18 13.1543 19.7377 13.3724 22.0138L14.7562 36.4559C14.8869 37.8199 13.8145 39 12.4442 39H5.5425C4.17704 39 3.1062 37.8278 3.22939 36.4679L4.53675 22.0367Z",
+        "M4.58226 19.0392C4.76304 16.7586 6.66653 15 8.95434 15C11.2345 15 13.1343 16.7472 13.3248 19.0194L14.789 36.4834C14.9026 37.8379 13.8338 39 12.4745 39H5.51397C4.1589 39 3.09158 37.8447 3.19866 36.4939L4.58226 19.0392Z"
+    ]
 
-export const FactoryIcon = () => {
-    const chimneyProgress = useMotionValue(0);
-    const chimneyPath = useTransform(chimneyProgress, [0, 1], [...CHIMNEY_PATHS]);
-    // const smokeY = useTransform(chimneyProgress, [0, 1], [0, 2.98]);
+    const barProgress = useMotionValue(0);
+    const barPath = useTransform(barProgress, [0, 1], [...CHIMNEY_PATHS]);
 
-    const squeezeChimney = () => {
-        animate(chimneyProgress, 1, SQUEEZE_TRANSITION);
-    };
+    useEffect(() => {
+        animate(barProgress, [0, 1, 0], {
+            ease: easeOut,
+            duration: 1.6,
+            times: [0, 0.32, 0.9],
+            repeat: Infinity,
+            repeatDelay: 1.2
+        });
 
-    const releaseChimney = () => {
-        animate(chimneyProgress, 0, RELEASE_TRANSITION);
-    };
+        // scope.current.querySelectorAll("[data-animate='line']").forEach((line: any) => {
+        //     animate(
+        //       line,
+        //       {
+        //         strokeDashoffset: 0,
+        //       },
+        //       {
+        //         delay: 0.04,
+        //         duration: 0.53,
+        //         times: [0.7, 0.9],
+        //       },
+        //     );
+        //   });
+
+
+    }, []);
 
     return (
-        <motion.svg
-            width="40"
-            height="40"
-            viewBox="0 0 40 40"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            onHoverStart={squeezeChimney}
-            onHoverEnd={releaseChimney}
-            className="cursor-pointer"
-        >
-            <path d="M24.1402 21.3132C23.8088 22.0693 24.7028 22.7697 25.3577 22.2669L35.0013 14.8624C35.3617 14.5856 35.8035 14.4355 36.258 14.4355C37.2201 14.4355 38 15.2154 38 16.1775V33.6694C38 34.6315 37.2201 35.4114 36.258 35.4114H9.04028C8.07823 35.4114 7.29834 34.6315 7.29834 33.6694V31.3221C7.29834 29.5233 8.79382 28.086 10.0781 26.8265L22.2526 15.0548C22.6634 14.6576 23.2124 14.4355 23.7838 14.4355C25.3729 14.4355 26.4391 16.067 25.8013 17.5224L24.1402 21.3132Z" fill="#9295FE" />
-            <motion.g initial={false}>
-                <path d="M4.68543 14.399C3.88705 13.8547 3.95963 12.4031 4.86689 11.024C5.77415 9.645 4.54028 7.75791 3.92334 7.28613" stroke="#9295FE" strokeLinecap="round" />
-                <path d="M11.2133 14.4354C12.0116 13.8911 11.9391 12.4394 11.0318 11.0604C10.1245 9.68138 11.3584 7.79428 11.9753 7.32251" stroke="#9295FE" strokeLinecap="round" />
-                <path d="M8.14733 13.6371C7.74812 13.0565 7.07481 11.2794 7.85701 9.89919C9.50005 7 5.5 7.5 7.99995 4" stroke="#9295FE" strokeWidth="1.3" strokeLinecap="round" />
+        <motion.svg width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M25.8418 24.8776C25.5105 25.6338 26.4045 26.3341 27.0593 25.8313L36.7029 18.4268C37.0634 18.15 37.5052 18 37.9597 18C38.9217 18 39.7016 18.7799 39.7016 19.7419V37.2339C39.7016 38.1959 38.9217 38.9758 37.9597 38.9758H10.7419C9.77989 38.9758 9 38.1959 9 37.2339V34.8866C9 33.0878 10.4955 31.6504 11.7798 30.3909L23.9542 18.6192C24.365 18.222 24.914 18 25.4854 18C27.0745 18 28.1408 19.6314 27.503 21.0869L25.8418 24.8776Z" fill="#9295FE" />
+            <AnimatePresence mode="popLayout">
+            <motion.g ref={scope} >
+                <motion.path
+                    data-animate="line"
+                    d="M5.76209 14.1129C4.96371 13.5685 5.03629 12.1169 5.94355 10.7379C6.85081 9.35887 5.61694 7.47177 5 7"
+                    stroke="#9295FE"
+                    strokeLinecap="round"
+                    strokeDasharray="7.11px 8.5px" 
+                    strokeDashoffset="8px"
+                    pathLength="7.11"
+                    initial={{ strokeDashoffset: "8px" }}
+                    animate={{ strokeDashoffset: ["8px", "0px", "-8px"] }}
+                    transition={{ duration: 1, ease: easeOut, repeat: Infinity, repeatDelay: 1.96, delay: 0.4 }}
+                />
+                <motion.path
+                    data-animate="line"
+                    d="M8.99328 13.6371C8.59407 13.0565 7.92075 11.2794 8.70295 9.89919C10.346 7 6.34595 7.5 8.8459 4"
+                    stroke="#9295FE"
+                    strokeWidth="1.2"
+                    strokeLinecap="round"
+                    strokeDasharray="9.64px 11px" 
+                    strokeDashoffset="10px"
+                    pathLength="9.64"
+                    initial={{ strokeDashoffset: "10px" }}
+                    animate={{ strokeDashoffset: ["10px", "0px", "-10px"] }}
+                    transition={{ duration: 0.9, ease: easeOut, repeat: Infinity, repeatDelay: 1.9, delay: 0.66 }}
+                />
+                <motion.path
+                    data-animate="line"
+                    d="M12.5035 14.1129C13.3019 13.5685 13.2293 12.1169 12.3221 10.7379C11.4148 9.35887 12.6487 7.47177 13.2656 7"
+                    stroke="#9295FE"
+                    strokeLinecap="round"
+                    strokeDasharray="7.11px 8.5px" 
+                    strokeDashoffset="8px"
+                    pathLength="7.11"
+                    initial={{ strokeDashoffset: "8px" }}
+                    animate={{ strokeDashoffset: ["8px", "0px", "-8px"] }}
+                    transition={{ duration: 1, ease: easeOut, repeat: Infinity, repeatDelay: 1.96, delay: 0.77 }}
+                />
+                
             </motion.g>
-            <motion.path d={chimneyPath} fill="#363DEE" />
-            <rect x="17.605" y="27.7178" width="5.08065" height="3.19355" rx="1.16129" fill="#4E28F5" />
-            <rect x="28.7097" y="27.7178" width="5.08065" height="3.19355" rx="1.16129" fill="#4E28F5" />
+            </AnimatePresence>
+
+            <motion.path d={barPath} fill="#363DEE" />
+            <rect x="19.605" y="30.7178" width="5.08065" height="3.19355" rx="1.16129" fill="#4E28F5" />
+            <rect x="30.7097" y="30.7178" width="5.08065" height="3.19355" rx="1.16129" fill="#4E28F5" />
         </motion.svg>
     )
 }
+
